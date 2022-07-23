7c0e0cd05239fde56e68d23bbdfbc7cc9986d845
progenor <nero6126@gmail.com>
2022-07-23 19:41:04 +0300

    language update

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,38 +1,17 @@
-import sty from "../styles/about.module.scss";
-import React, { useEffect } from "react";
+import sty from '../styles/about.module.scss';
+import React, { useEffect } from 'react';
 
-import useTranslation from "next-translate/useTranslation";
+import useTranslation from 'next-translate/useTranslation';
 
-const About = ({ title }) => {
-  let { t, lang } = useTranslation();
+const About = () => {
+  let { t, lang } = useTranslation('about');
 
   return (
     <div className={sty.main_container}>
       <div className={sty.about_main} id="about">
-        <h1>{t("common:title")}</h1>
+        <h1>{t('common:title')}</h1>
         <div className={sty.about_container}>
-          <p>
-            Lorem ipsum est cillum labore qui non incididunt veniam magna enim
-            duis labore do nostrud consectetur sit eu nisi consectetur aute
-            laboris ullamco deserunt irure labore aute do nulla laboris proident
-            ullamco anim cillum laborum aliqua ea laborum in voluptate esse
-            voluptate in esse deserunt enim eu pariatur in minim voluptate
-            labore tempor dolor consectetur deserunt elit proident ea incididunt
-            ea magna culpa labore ad do esse eu dolor adipisicing ut enim
-            deserunt non in reprehenderit deserunt aliquip adipisicing fugiat in
-            ea et occaecat ex sint occaecat in in velit aliquip dolore eu in
-            fugiat incididunt laboris ut consectetur et velit et in anim ullamco
-            cillum enim reprehenderit minim est nostrud in nisi exercitation
-            officia tempor magna in est in magna velit do adipisicing sit
-            adipisicing in sit do do mollit proident dolor aliquip proident sit
-            commodo pariatur officia in pariatur in commodo exercitation commodo
-            qui nisi nulla proident veniam nostrud dolore sint esse velit in
-            reprehenderit incididunt dolor non incididunt et est mollit dolore
-            consequat proident ullamco ea ex eiusmod culpa ea proident nisi ad
-            officia aute aliqua sed sit minim tempor dolore laborum qui pariatur
-            id ut proident aliqua dolore laborum ullamco dolore culpa est enim
-            in exercitation
-          </p>
+          <p>{t('description')}</p>
         </div>
         <img src="/assets/svgs/about_us.svg" alt="" />
       </div>
