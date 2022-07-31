dfd8be1932f8b9a9e77caad61de23fceb002e8ba
progenor <nero6126@gmail.com>
2022-07-31 23:34:53 +0300

    save

--- components/PlasSpacers.jsx
+++ components/PlasSpacers.jsx
@@ -501,30 +501,410 @@ const PlasSpacers = () => {
       <div className={styy.table1}>
         <h2 id="cepd">Chamfer edge profiles D – length 2,5 m</h2>
         <IMG img1="cepd1.jpg" img2="cepd2.jpg" />
+        <div className={styy.table}>
+          <table>
+            <thead>
+              <tr>
+                <th>Type</th>
+                <th>Dimension a (mm)</th>
+                <th>Dimension b (mm)</th>
+                <th>Pack size - bundle (m)</th>
+                <th>Pack size - pallet (m)</th>
+                <th>Bundle (kg)</th>
+                <th>Pallet (kg)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>D 06 full</th>
+                <td>6</td>
+                <td>8</td>
+                <td>100</td>
+                <td>10000</td>
+                <td>5,0</td>
+                <td>500</td>
+              </tr>
+              <tr>
+                <th>D 10</th>
+                <td>10</td>
+                <td>15</td>
+                <td>100</td>
+                <td>6500</td>
+                <td>5,2</td>
+                <td>338</td>
+              </tr>
+              <tr>
+                <th>D 15</th>
+                <td>15</td>
+                <td>21</td>
+                <td>100</td>
+                <td>5400</td>
+                <td>8,0</td>
+                <td>432</td>
+              </tr>
+              <tr>
+                <th>D 20</th>
+                <td>20</td>
+                <td>28</td>
+                <td>100</td>
+                <td>4900</td>
+                <td>12,0</td>
+                <td>588</td>
+              </tr>
+              <tr>
+                <th>D 25</th>
+                <td>25</td>
+                <td>35</td>
+                <td>50</td>
+                <td>2450</td>
+                <td>8,3</td>
+                <td>407</td>
+              </tr>
+              <tr>
+                <th>D 30</th>
+                <td>30</td>
+                <td>40</td>
+                <td>50</td>
+                <td>2450</td>
+                <td>11,8</td>
+                <td>566</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
       <div className={styy.table1}>
         <h2 id="cepdfv">
           Chamfer edge profiles with nailing flange DFV full - length 2,5 m
         </h2>
         <IMG img1="cepdfv1.jpg" img2="cepdfv2.jpg" />
+        <div className={styy.table}>
+          <table>
+            <thead>
+              <tr>
+                <th>Type</th>
+                <th>Dimension a (mm)</th>
+                <th>Dimension b (mm)</th>
+                <th>Dimension c (mm)</th>
+                <th>Pack size - bundle (m)</th>
+                <th>Pallet (m)</th>
+                <th>Pallet (kg)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>DFV 04</th>
+                <td>3,8</td>
+                <td>5</td>
+                <td>23</td>
+                <td>100</td>
+                <td>6500</td>
+                <td>377</td>
+              </tr>
+              <tr>
+                <th>DFV 06</th>
+                <td>6</td>
+                <td>9,5</td>
+                <td>23</td>
+                <td>100</td>
+                <td>6500</td>
+                <td>390</td>
+              </tr>
+              <tr>
+                <th>DFV 07</th>
+                <td>7</td>
+                <td>11</td>
+                <td>23</td>
+                <td>100</td>
+                <td>6000</td>
+                <td>420</td>
+              </tr>
+              <tr>
+                <th>DFV 10-2</th>
+                <td>9,5</td>
+                <td>14</td>
+                <td>24</td>
+                <td>100</td>
+                <td>5400</td>
+                <td>540</td>
+              </tr>
+              <tr>
+                <th>DFV 10-3</th>
+                <td>11</td>
+                <td>15</td>
+                <td>27</td>
+                <td>100</td>
+                <td>5400</td>
+                <td>648</td>
+              </tr>
+              <tr>
+                <th>DFV 10-4</th>
+                <td>11</td>
+                <td>16</td>
+                <td>24</td>
+                <td>100</td>
+                <td>5400</td>
+                <td>648</td>
+              </tr>
+              <tr>
+                <th>DFV 15</th>
+                <td>15</td>
+                <td>23</td>
+                <td>33</td>
+                <td>100</td>
+                <td>4800</td>
+                <td>864</td>
+              </tr>
+              <tr>
+                <th>DFV 10 flexible</th>
+                <td>9,5</td>
+                <td>14</td>
+                <td>26</td>
+                <td>250</td>
+                <td>5000</td>
+                <td>360</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
       <div className={styy.table1}>
         <h2 id="cepdfh">
           Chamfer edge profiles with nailing flange DFH hollow –length 2,5 m
         </h2>
         <IMG img1="cepdfh1.jpg" img2="cepdfh2.jpg" img3="cepdfh3.jpg" />
+        <div className={styy.table}>
+          <table>
+            <thead>
+              <tr>
+                <th>Type</th>
+                <th>Dimension a (mm)</th>
+                <th>Dimension b (mm)</th>
+                <th>Dimension c (mm)</th>
+                <th>Pack size - bundle (m)</th>
+                <th>Pallet (m)</th>
+                <th>Pallet (kg)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>DFH 10</th>
+                <td>11</td>
+                <td>15</td>
+                <td>23</td>
+                <td>100</td>
+                <td>5000</td>
+                <td>400</td>
+              </tr>
+              <tr>
+                <th>DFH 15</th>
+                <td>15</td>
+                <td>22</td>
+                <td>29</td>
+                <td>100</td>
+                <td>4900</td>
+                <td>573</td>
+              </tr>
+              <tr>
+                <th>DFH 20</th>
+                <td>20</td>
+                <td>27</td>
+                <td>35</td>
+                <td>50</td>
+                <td>2400</td>
+                <td>523</td>
+              </tr>
+              <tr>
+                <th>DFH 25</th>
+                <td>25</td>
+                <td>36</td>
+                <td>42</td>
+                <td>50</td>
+                <td>2400</td>
+                <td>715</td>
+              </tr>
+              <tr>
+                <th>DFH 30</th>
+                <td>30</td>
+                <td>42</td>
+                <td>52</td>
+                <td>50</td>
+                <td>1800</td>
+                <td>612</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
       <div className={styy.table1}>
         <h2 id="sppp">Sealing profile PP</h2>
         <IMG img1="sppp1.jpg" />
+        <div className={styy.table}>
+          <table>
+            <thead>
+              <tr>
+                <th>Type</th>
+                <th>Hypotenuse (mm)</th>
+                <th>Cathetus (mm)</th>
+                <th>Pack size - bundle (m)</th>
+                <th>Bundle (kg)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>Sealing profile PP red</th>
+                <td>15</td>
+                <td>23</td>
+                <td>100</td>
+                <td>11</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
       <div className={styy.table1}>
         <h2 id="rsdk">Ring spacer diskus DK</h2>
         <IMG img1="rsdk1.jpg" />
+        <div className={styy.table}>
+          <table>
+            <thead>
+              <tr>
+                <th>Type</th>
+                <th>Height (mm)</th>
+                <th>Pack size - bag (sets)</th>
+                <th>Pallet (sets)</th>
+                <th>Bag (kg)</th>
+                <th>Pallet (kg)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>DK 15</th>
+                <td>15</td>
+                <td>60 sets (set = 6 rings)</td>
+                <td>on request</td>
+                <td>6,5</td>
+                <td>312</td>
+              </tr>
+              <tr>
+                <th>DK 20</th>
+                <td>20</td>
+                <td>50 sets (set = 6 rings)</td>
+                <td>on request</td>
+                <td>6,8</td>
+                <td>245</td>
+              </tr>
+              <tr>
+                <th>DK 25</th>
+                <td>25</td>
+                <td>40 sets (set = 6 rings)</td>
+                <td>on request</td>
+                <td>7,0</td>
+                <td>324</td>
+              </tr>
+              <tr>
+                <th>DK 30</th>
+                <td>30</td>
+                <td>34 sets (set = 6 rings)</td>
+                <td>on request</td>
+                <td>7,5</td>
+                <td>180</td>
+              </tr>
+              <tr>
+                <th>DK 35</th>
+                <td>35</td>
+                <td>30 sets (set = 6 rings)</td>
+                <td>on request</td>
+                <td> - </td>
+                <td> - </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
       <div className={styy.table1}>
         <h2 id="rde">Rhombus DE</h2>
         <IMG img1="rde1.jpg" img2="rde2.jpg" />
+        <div className={styy.table}>
+          <table>
+            <thead>
+              <tr>
+                <th>Type</th>
+                <th>Height (mm)</th>
+                <th>Bundle (pcs)</th>
+                <th>Pallet (pcs)</th>
+                <th>Bundle (kg)</th>
+                <th>Pallet (kg)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>DE 15</th>
+                <td>15</td>
+                <td>60</td>
+                <td>9240</td>
+                <td>1,7</td>
+                <td>264</td>
+              </tr>
+              <tr>
+                <th>DE 20</th>
+                <td>20</td>
+                <td>60</td>
+                <td>9240</td>
+                <td>1,9</td>
+                <td>293</td>
+              </tr>
+              <tr>
+                <th>DE 25</th>
+                <td>25</td>
+                <td>60</td>
+                <td>9240</td>
+                <td>2,1</td>
+                <td>324</td>
+              </tr>
+              <tr>
+                <th>DE 30</th>
+                <td>30</td>
+                <td>60</td>
+                <td>9240</td>
+                <td>2,3</td>
+                <td>355</td>
+              </tr>
+              <tr>
+                <th>DE 35</th>
+                <td>35</td>
+                <td>60</td>
+                <td>7920</td>
+                <td>2,5</td>
+                <td>330</td>
+              </tr>
+              <tr>
+                <th>DE 40</th>
+                <td>40</td>
+                <td>60</td>
+                <td>7920</td>
+                <td>2,7</td>
+                <td>357</td>
+              </tr>
+              <tr>
+                <th>DE 45</th>
+                <td>45</td>
+                <td>60</td>
+                <td>5280</td>
+                <td>2,9</td>
+                <td>256</td>
+              </tr>
+              <tr>
+                <th>DE 50</th>
+                <td>50</td>
+                <td>60</td>
+                <td>5280</td>
+                <td>3,1</td>
+                <td>273</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
