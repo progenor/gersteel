2c1921cab657876cd9829f6543dfb84def3c1041
progenor <nero6126@gmail.com>
2022-08-06 16:45:19 +0300

    language fix 2

--- components/NavNormal.jsx
+++ components/NavNormal.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import Link from 'next/link';
+import { useState } from "react";
+import Link from "next/link";
 
-import sty from '../styles/navbar.module.scss';
+import sty from "../styles/navbar.module.scss";
 // import { MdOutlineKeyboardArrowDown } from "react-icons/md";
-import { IoMdArrowDropdown } from 'react-icons/io';
-import NavProdItems from './NavProdItems';
+import { IoMdArrowDropdown } from "react-icons/io";
+import NavProdItems from "./NavProdItems";
 
-import useTranslation from 'next-translate/useTranslation';
+import useTranslation from "next-translate/useTranslation";
 
 const NavNormal = () => {
   const { t, lang } = useTranslation();
@@ -20,15 +20,16 @@ const NavNormal = () => {
             onClick={() => {
               setIsactive(!isactive);
             }}
+            style={{ textTransform: "uppercase" }}
           >
-            {t('common:products')}
+            {t("common:products")}
             {/* <MdOutlineKeyboardArrowDown color="white" size={20} style={{}} /> */}
             <IoMdArrowDropdown color="white" size={20} />
           </a>
         </div>
-        {isactive ? <NavProdItems /> : ''}
-        <Link href="/About">{t('common:about')}</Link>
-        <Link href="/Contact">{t('common:contact')}</Link>
+        {isactive ? <NavProdItems /> : ""}
+        <Link href="/About">{t("common:about")}</Link>
+        <Link href="/Contact">{t("common:contact")}</Link>
       </div>
     </>
   );
