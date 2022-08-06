81b8b60dcac490b87a635e71e44a035d4fcd87d9
progenor <nero6126@gmail.com>
2022-08-06 16:40:57 +0300

    language fix

--- pages/About.jsx
+++ pages/About.jsx
@@ -4,14 +4,14 @@ import React, { useEffect } from "react";
 import useTranslation from "next-translate/useTranslation";
 
 const About = () => {
-  const { t } = useTranslation("about");
+  const { t } = useTranslation();
 
   return (
     <div className={sty.main_container}>
       <div className={sty.about_main} id="about">
         <h1>{t("common:about")}</h1>
         <div className={sty.about_container}>
-          <p>{t("description")}</p>
+          <p>{t("about:description")}</p>
         </div>
         <img src="/assets/svgs/about_us.svg" alt="" />
       </div>
